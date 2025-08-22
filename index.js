c5fc1b317235fc14b095da0ba8754b6d08ee51be
maomixo-glitch <maomixo@gmail.com>
2025-08-22 10:31:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -289,7 +289,7 @@ app.post('/webhook', line.middleware(config), async (req, res) => {
 });
 
 // ✅ 每週六 23:56 推播
-const GROUP_ID = 'Cd564f9c185a66079523681dece397715'; // 你的群組 ID
+const GROUP_ID = 'C0b50f32fbcc66de32339fe91f5240d7f'; // 你的群組 ID
 cron.schedule('56 23 * * 6', async () => {
   try {
     await client.pushMessage(GROUP_ID, {
